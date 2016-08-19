2b4e816d8e6b2ed7293bbe6dd5aa53be7cf4ee1b
Ghost1227 <dgriffiths@section214.com>
2016-08-18 21:08:46 -0500

    Fix edd_is_checkout bug

--- includes/admin/settings/register.php
+++ includes/admin/settings/register.php
@@ -14,6 +14,21 @@ if( ! defined( 'ABSPATH' ) ) {
 
 
 /**
+ * Add settings section
+ *
+ * @since       1.0.2
+ * @param       array $sections The existing extensions sections
+ * @return      array The modified extensions settings
+ */
+function edd_conditional_gateways_add_settings_section( $sections ) {
+	$sections['conditional-gateways'] = __( 'Conditional Gateways', 'edd-conditional-gateways' );
+
+	return $sections;
+}
+add_filter( 'edd_settings_sections_gateways', 'edd_conditional_gateways_add_settings_section' );
+
+
+/**
  * Add settings
  *
  * @since       1.0.1
@@ -22,22 +37,24 @@ if( ! defined( 'ABSPATH' ) ) {
  */
 function edd_conditional_gateways_add_settings( $settings ) {
 	$new_settings = array(
-		array(
-			'id'    => 'edd_conditional_gateways_settings',
-			'name'  => '<strong>' . __( 'Conditional Gateways Settings', 'edd-conditional-gateways' ) . '</strong>',
-			'desc'  => '',
-			'type'  => 'header'
-		),
-		array(
-			'id'    => 'edd_conditional_gateways_checkout_error',
-			'name'  => __( 'Checkout Error Message', 'edd-conditional-gateways' ),
-			'desc'  => __( 'The error message to display if no gateways are available due to the cart contents.', 'edd-conditional-gateways' ),
-			'type'  => 'text',
-			'std'   => __( 'Your cart contents have resulted in no gateways being available. Please remove an item from your cart and try again.', 'edd-conditional-gateways' )
-		)
+		'conditional-gateways' => apply_filters( 'edd_conditional_gateways_settings', array(
+			array(
+				'id'    => 'edd_conditional_gateways_settings',
+				'name'  => '<strong>' . __( 'Conditional Gateways Settings', 'edd-conditional-gateways' ) . '</strong>',
+				'desc'  => '',
+				'type'  => 'header'
+			),
+			array(
+				'id'    => 'edd_conditional_gateways_checkout_error',
+				'name'  => __( 'Checkout Error Message', 'edd-conditional-gateways' ),
+				'desc'  => __( 'The error message to display if no gateways are available due to the cart contents.', 'edd-conditional-gateways' ),
+				'type'  => 'text',
+				'std'   => __( 'Your cart contents have resulted in no gateways being available. Please remove an item from your cart and try again.', 'edd-conditional-gateways' )
+			)
+		) )
 	);
 
 	return array_merge( $settings, $new_settings );
 }
 
-add_filter( 'edd_settings_extensions', 'edd_conditional_gateways_add_settings' );
+add_filter( 'edd_settings_gateways', 'edd_conditional_gateways_add_settings' );
